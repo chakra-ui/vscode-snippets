8a47b49194b7cbca22d521e9824bd0d738ee3d0d
Segun Adebayo <joseshegs@gmail.com>
2020-05-27 18:12:40 +0400

    chore: updates

--- playground.js
+++ playground.js
@@ -1,28 +1,33 @@
-import { Accordion, AccordionItem, AccordionHeader, AccordionPanel, AccordionIcon} from "@chakra-ui/core"
+import {
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/core";
 
 <div>
-<Accordion>
+  <Accordion>
     <AccordionItem>
-        <AccordionHeader>
-            <Box flex="1" textAlign="left">
-            Section 1 title
-            </Box>
-            <AccordionIcon />
-        </AccordionHeader>
-        <AccordionPanel pb={4}>
+      <AccordionHeader>
+        <Box flex="1" textAlign="left">
+          Section 1 title
+        </Box>
+        <AccordionIcon />
+      </AccordionHeader>
+      <AccordionPanel pb={4}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat.
-        </AccordionPanel>
+      </AccordionPanel>
     </AccordionItem>
-</Accordion>
+  </Accordion>
 
-<Alert status="error">
+  <Alert status="error">
     <AlertIcon />
     <AlertTitle mr={2}>Your browser is outdated!</AlertTitle>
     <AlertDescription>Your Chakra experience may be degraded.</AlertDescription>
     <CloseButton position="absolute" right="8px" top="8px" />
-</Alert>
-
-</div>
+  </Alert>
+</div>;
